2e2bffb33e4c77270b4e29585aa20550db143cae
Joe Whelton <joe.whelton@mycit.ie>
2020-05-01 21:38:05 +0100

    Displaying statistics to screen as well as writing them

--- Functions/commonFunctions.py
+++ Functions/commonFunctions.py
@@ -95,6 +95,7 @@ def generate_statistics_menu(modules):
 
 def generate_statistics(module):
     today = datetime.datetime.now()
+    filename = f"{DATA_PATH}{module.code}{today.strftime('%Y-%m-%d')}.txt"
     number_of_students = len(module.students)
     number_of_classes = module.students[0].present + module.students[0].absent + module.students[0].excused
     total_attendance, top_attendance = 0, 0
@@ -113,11 +114,11 @@ def generate_statistics(module):
         if student.present == top_attendance:
             best_attendance.append(student.name)
 
-    connection = open(f"{DATA_PATH}{module.code}{today.strftime('%Y-%m-%d')}.txt", "w+")
+    connection = open(filename, "w+")
     connection.write(f"Module: {module.code} {module.title}\n")
     connection.write(f"Number of students: {number_of_students}\n")
     connection.write(f"Number of classes: {number_of_classes}\n")
-    connection.write(f"Average attendance: {(total_attendance / number_of_students):.2f}\n")
+    connection.write(f"Average attendance: {(total_attendance / number_of_students):.1f}\n")
     connection.write("Low Attender(s): under 70.0 %\n")
     write_tabbed_list(low_attendance, connection)
     connection.write("Non Attender(s):\n")
@@ -125,6 +126,10 @@ def generate_statistics(module):
     connection.write("Best Attender(s):\n")
     connection.write(f"\tAttended{top_attendance}/{number_of_classes} days\n")
     write_tabbed_list(best_attendance, connection)
+
+    connection.close()
+    connection = open(filename)
+    print(connection.read())
 
 
 def write_tabbed_list(list, connection):
