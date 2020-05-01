55a321e52727bbdf6a1b40a5f19f7e7d5b58f14f
Joe Whelton <joe.whelton@mycit.ie>
2020-05-01 15:37:23 +0100

    Initial project setup

---
+++ Classes/Modules.py
@@ -0,0 +1,3 @@
+class Module:
+    def __init__(self, title):
+        self.title = title
---
+++ Classes/Students.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, present, absent, excused):
+        self.name = name
+        self.present = present
+        self.absent = absent
+        self.excused = excused
+
+
