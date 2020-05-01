b45e5fd074e98bf56c528a5b732dd6bda58235ed
Joe Whelton <joe.whelton@mycit.ie>
2020-05-01 17:39:22 +0100

    Added login functionality to Main

--- Functions/commonFunctions.py
+++ Functions/commonFunctions.py
@@ -1,4 +1,4 @@
-def menuHading(heading_text):
-    print("*" * len(heading_text))
+def menu_heading(heading_text):
     print(heading_text)
-    print("*" * len(heading_text))
+    print("-" * len(heading_text))
+    print("\n")
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from config import *
+from Functions.commonFunctions import *
+from Classes.Users import Users
 from Classes.Modules import Modules
 
-def login():
-    print()
+
+def login(users):
+    username = input("Name: ")
+    password = input("Password: ")
+    return users.validate(username, password)
 
 
 def main():
-    login()
+    users = Users("Login_data.txt")
+    users.load_data()
+
+    while not login(users):
+        print("Access Denied")
+
 
 
 main()
