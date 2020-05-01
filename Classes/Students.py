66c060424f86765cee3f475e33e0624dc9792f13
Joe Whelton <joe.whelton@mycit.ie>
2020-05-01 21:16:44 +0100

    Added Generate Statistics functionality - writing to file only

--- Classes/Students.py
+++ Classes/Students.py
@@ -8,4 +8,3 @@ class Student:
         self.absent = absent
         self.excused = excused
 
-
--- Functions/commonFunctions.py
+++ Functions/commonFunctions.py
@@ -1,3 +1,6 @@
+from config import *
+import datetime
+
 def menu_heading(heading_text):
     print(heading_text)
     print("-" * len(heading_text))
@@ -23,7 +26,7 @@ def main_menu(modules):
         if choice == "1":
             record_attendance_menu(modules)
         elif choice == "2":
-            generate_statistics(modules)
+            generate_statistics_menu(modules)
         elif choice == "3":
             return
         else:
@@ -71,5 +74,62 @@ def record_attendance(module):
     print(f"{module.code}.txt updated with latest attendance records")
 
 
-def generate_statistics(modules):
-    return 0
+def generate_statistics_menu(modules):
+    choice = -1
+    loop = True
+    while loop:
+        menu_heading("Module Record System(Attendance) - Choose a Module")
+        for i, mod in enumerate(modules.module):
+            print(f"{i + 1} - {mod.code} {mod.title}")
+        print("x - Back to Main Menu")
+        choice = input(">")
+
+        if choice.lower() == "x":
+            return
+        try:
+            module = modules.module[int(choice) - 1]
+            generate_statistics(module)
+        except:
+            print("Invalid choice")
+
+
+def generate_statistics(module):
+    today = datetime.datetime.now()
+    number_of_students = len(module.students)
+    number_of_classes = module.students[0].present + module.students[0].absent + module.students[0].excused
+    total_attendance, top_attendance = 0, 0
+    low_attendance, non_attendance, best_attendance = [], [], []
+
+    for student in module.students:
+        total_attendance += student.present
+        if student.present < (number_of_classes * 0.7):
+            low_attendance.append(student.name)
+        if student.present == 0:
+            non_attendance.append(student.name)
+        if student.present > top_attendance:
+            top_attendance = student.present
+
+    for student in module.students:
+        if student.present == top_attendance:
+            best_attendance.append(student.name)
+
+    connection = open(f"{DATA_PATH}{module.code}{today.strftime('%Y-%m-%d')}.txt", "w+")
+    connection.write(f"Module: {module.code} {module.title}\n")
+    connection.write(f"Number of students: {number_of_students}\n")
+    connection.write(f"Number of classes: {number_of_classes}\n")
+    connection.write(f"Average attendance: {(total_attendance / number_of_students):.2f}\n")
+    connection.write("Low Attender(s): under 70.0 %\n")
+    write_tabbed_list(low_attendance, connection)
+    connection.write("Non Attender(s):\n")
+    write_tabbed_list(non_attendance, connection)
+    connection.write("Best Attender(s):\n")
+    connection.write(f"\tAttended{top_attendance}/{number_of_classes} days\n")
+    write_tabbed_list(best_attendance, connection)
+
+
+def write_tabbed_list(list, connection):
+    if len(list) == 0 :
+        connection.write("\tNONE\n")
+    else:
+        for x in range(len(list)):
+            connection.write(f"\t{list[x]}\n")
